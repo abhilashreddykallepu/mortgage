972dec543dd309369e301a4843dd8e1ed8f80c31
ramkrishna880 <ramkrishna880@gmail.com>
2014-03-04 14:11:54 +0530

    added shit

--- MortagageApp/ViewController.m
+++ MortagageApp/ViewController.m
@@ -23,7 +23,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
-        //added some shit
+        //added some shit  jhgvjhgv
     }
     return self;
 }
